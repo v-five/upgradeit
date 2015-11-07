209ee3075ca699a56a18042c2210bda0055d58f3
v-five <vityka.vv@gmail.com>
2015-11-07 21:20:24 +0200

    Project Edit

--- UpgradeIt/Controllers/ProjectController.cs
+++ UpgradeIt/Controllers/ProjectController.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using umbraco.BusinessLogic;
+using umbraco.cms.businesslogic.web;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
 
 namespace UpgradeIt.Controllers
 {
-    public class ProjectController : Controller
+    public class ProjectEditController : RenderMvcController
     {
-        // GET: Project
-        public ActionResult Index()
+        [HttpGet]
+        public override ActionResult Index(RenderModel model)
         {
-            return View();
+            if (umbraco.BusinessLogic.User.GetCurrent() != null)
+            {
+                return base.View(model);
+            }
+
+            return this.Redirect(umbraco.library.NiceUrl(id));
+        }
+
+        [HttpPost]
+        public override ActionResult Index(RenderModel model)
+        {
+            var id = Convert.ToInt32(Request.Params["id"]);
+            if (umbraco.BusinessLogic.User.GetCurrent() != null)
+            {
+                var project = new Document(id);
+
+                project.getProperty("title").Value = Request["title"];
+                project.getProperty("description").Value = Request["description"];
+                project.getProperty("image").Value = Request["image"];
+                project.getProperty("allowComments").Value = Request["allowComments"];
+                project.getProperty("projectType").Value = Request["projectType"];
+                project.getProperty("projectArea").Value = Request["projectArea"];
+
+                project.SaveAndPublish(umbraco.BusinessLogic.User.GetCurrent());
+            }
+
+            return this.Redirect(umbraco.library.NiceUrl(id));
         }
     }
 }   
