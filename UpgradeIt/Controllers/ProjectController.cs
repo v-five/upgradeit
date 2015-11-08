92b0769b0f1bbd3c0be63657386a13b05846efdb
v-five <vityka.vv@gmail.com>
2015-11-08 04:14:58 +0200

    Added Create project page

--- UpgradeIt/Controllers/ProjectController.cs
+++ UpgradeIt/Controllers/ProjectController.cs
@@ -9,6 +9,9 @@ using umbraco.cms.businesslogic.media;
 using umbraco.cms.businesslogic.web;
 using Umbraco.Web.Models;
 using Umbraco.Web.Mvc;
+using umbraco.NodeFactory;
+using umbraco.cms.businesslogic.member;
+using umbraco.cms.businesslogic.template;
 
 namespace UpgradeIt.Controllers
 {
@@ -16,41 +19,78 @@ namespace UpgradeIt.Controllers
     {
         public override ActionResult Index(RenderModel model)
         {
-            var id = Convert.ToInt32(Request.Params["id"]);
-            var project = new Document(id);
-
-            var member = umbraco.cms.businesslogic.member.Member.GetCurrentMember();
-            var authorId = project.getProperty("author").Value;
-            if (member != null && member.Id.Equals(authorId))
+            if (model.Content.TemplateId.Equals(Template.GetTemplateIdFromAlias("ProjectCreate")))
             {
-                if(Request.HttpMethod.Equals("GET"))
-                    return base.View(model);
-
-                if (!string.IsNullOrWhiteSpace(Request["title"]))
-                    project.getProperty("title").Value = Request["title"];
-                if (!string.IsNullOrWhiteSpace(Request["description"]))
-                    project.getProperty("description").Value = Request["description"];
-                if (!string.IsNullOrWhiteSpace(Request["projectType"]))
-                    project.getProperty("projectType").Value = Convert.ToInt32(Request["projectType"]);
-                if (!string.IsNullOrWhiteSpace(Request["area"]))
-                    project.getProperty("area").Value = Convert.ToInt32(Request["area"]);
-                project.getProperty("allowComments").Value = !string.IsNullOrWhiteSpace(Request["allowComments"]) && Request["allowComments"].ToLower().Equals("on");
-
-                if (Request.Files.Count > 0 && Request.Files[0].ContentLength > 0)
+                var member = Member.GetCurrentMember();
+                if (member != null)
                 {
-                    var uploadedFile = Request.Files[0];
-                    var fileName = Path.GetFileName(uploadedFile.FileName);
-                    var fileSavePath = Server.MapPath("~/media/projects/" + fileName);
-                    uploadedFile.SaveAs(fileSavePath);
+                    if (Request.HttpMethod.Equals("GET"))
+                        return CurrentTemplate(model);
 
-                    project.getProperty("image").Value = "/media/projects/" + fileName;
+                    var project = CreateProject(member.User);
+                    if (project != null) return this.Redirect(umbraco.library.NiceUrl(project.Id));
                 }
 
-                project.SaveAndPublish(new umbraco.BusinessLogic.User(0));
-                umbraco.library.UpdateDocumentCache(id);
+                return this.Redirect(Request.UrlReferrer.AbsoluteUri);
+            }
+            else
+            {
+                var id = Convert.ToInt32(Request.Params["id"]);
+                var project = new Document(id);
+
+                var member = Member.GetCurrentMember();
+                var authorId = project.getProperty("author").Value;
+                if (member != null && member.Id.Equals(authorId))
+                {
+                    if (Request.HttpMethod.Equals("GET"))
+                        return base.View(model);
+
+                    SaveProject(project, member.User);
+                }
+
+                return this.Redirect(umbraco.library.NiceUrl(id));
+            }
+        }
+
+        private Document CreateProject(User user)
+        {
+            if (!string.IsNullOrWhiteSpace(Request["title"]))
+            {
+                var project = Document.MakeNew(Request["title"], DocumentType.GetByAlias("project"), user, new Node(-1).Id);
+                SaveProject(project, user);
+
+                return project;
             }
 
-            return this.Redirect(umbraco.library.NiceUrl(id));
+            return null;
+        }
+
+        private void SaveProject(Document project, User user)
+        {
+            if (!string.IsNullOrWhiteSpace(Request["title"]))
+            {
+                project.getProperty("title").Value = Request["title"];
+            }
+            if (!string.IsNullOrWhiteSpace(Request["description"]))
+                project.getProperty("description").Value = Request["description"];
+            if (!string.IsNullOrWhiteSpace(Request["projectType"]))
+                project.getProperty("projectType").Value = Convert.ToInt32(Request["projectType"]);
+            if (!string.IsNullOrWhiteSpace(Request["area"]))
+                project.getProperty("area").Value = Convert.ToInt32(Request["area"]);
+            project.getProperty("allowComments").Value = !string.IsNullOrWhiteSpace(Request["allowComments"]) && Request["allowComments"].ToLower().Equals("on");
+
+            if (Request.Files.Count > 0 && Request.Files[0].ContentLength > 0)
+            {
+                var uploadedFile = Request.Files[0];
+                var fileName = Path.GetFileName(uploadedFile.FileName);
+                var fileSavePath = Server.MapPath("~/media/projects/" + fileName);
+                uploadedFile.SaveAs(fileSavePath);
+
+                project.getProperty("image").Value = "/media/projects/" + fileName;
+            }
+
+            project.SaveAndPublish(user);
+            umbraco.library.UpdateDocumentCache(project.Id);
         }
     }
 }   
